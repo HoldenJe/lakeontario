ad52dabbc81240be9e3940ebe4a06b3a64edbae2
J. Holden <jeremy.holden@ontario.ca>
2021-04-11 12:50:19 -0400

    added FEED function to convert coordinate format

---
+++ R/FEED_functions.R
@@ -0,0 +1,19 @@
+### FEED Functions
+
+#' feed_convert_coords
+#' @description FEED stores coordinate as `DDMM.ddd`. This function converts
+#' that format to `DD.ddd` for plotting purposes. Note: Longitude will normally require
+#' the value to be multiplied by `-1 ` for sites in the western hemisphere
+#' @export
+#' @examples
+#' feed_convert_coords(7705.819) * (-1)
+#' feed_convert_coords(4402.984)
+
+feed_convert_coords <- function(feedcoord){
+  feedcoord <- as.character(feedcoord)
+  deg <- as.numeric(substr(feedcoord, 1,2))
+  mm <- as.numeric(substr(feedcoord, 3, nchar(feedcoord)))/60
+  DDddd <- deg + mm
+  DDddd
+}
+
