a2f6a163603855dd2d38a12f55c4e50fbd61474c
J. Holden <jeremy.holden@ontario.ca>
2021-11-09 07:46:50 -0500

    fixes name due to csv import problem

--- data-raw/ImportSPClookup.R
+++ data-raw/ImportSPClookup.R
@@ -1,6 +1,7 @@
 library(stringr)
 library(usethis)
 RV2FN_SPECIES <- read.csv("data-raw/SPCShort2SPECIESRVCAT.csv")
+names(RV2FN_SPECIES)[1] <- "SPC_SHORT"
 RV2FN_SPECIES$SPC <- str_pad(RV2FN_SPECIES$SPC, 3, pad = "0")
 
 use_data(RV2FN_SPECIES, overwrite = T)
