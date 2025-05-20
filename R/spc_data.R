1f009a3f1a1475422ccdfd2b3c026c716680e9cb
J. Holden <jeremy.holden@ontario.ca>
2025-05-20 14:55:22 -0400

    adds new species to spc list

---
+++ R/spc_data.R
@@ -0,0 +1,17 @@
+#' @title short species list
+#' @name spc_data
+#'
+#' @description data frame
+#'
+#' @format a data frame
+#'
+#' @examples
+#' \dontrun{
+#' data("spc_data")
+#' head(spc_data)
+#' }
+#' @seealso spc_data
+
+
+data("spc_data")
+spc_data
